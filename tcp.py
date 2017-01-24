14e60c24a7a4c48430f65346de34fc7cb0723997
fujunhan <fujun.han@intel.com>
2017-01-24 09:19:31 +0800

    Add simplest WebUI

--- tcp.py
+++ tcp.py
@@ -11,7 +11,8 @@ path.append(abspath(dirname(__file__)))
 from app.rtdeclient import RTDEClient
 from app.powerinspect import startServer
 
-class Transfer(Resource):
+
+class API(Resource):
     isLeaf = True
 
     def __init__(self):
@@ -19,20 +20,32 @@ class Transfer(Resource):
         self.exClient = RTDEClient(self.piHandler)
         Resource.__init__(self)
 
-    def render_GET(self, request):
-        if self.exClient.factory.running:
-            return "Running"
-        else:
-            return "Stopped"
+    def render_GET(self, _):
+        status, text = ("Running", "Start") if self.exClient.factory.running else ("Stopped", "Stop")
+
+        return """
+            <HTML>
+            <HEAD>
+            <META http-equiv="refresh" content="5" />
+            <TITLE>Touch Point Monitor Application</TITLE>
+            </HEAD>
+            <BODY>
+            <H1>{}</H1>
+            <FORM action="api" method="post">
+                <INPUT type="submit" value="Start">
+            </FORM>
+            </BODY>
+            </HTML>
+                """.format(status, text)
 
     def render_POST(self, _):
         self.exClient.startPolling()
-        return "Started"
+        return True
 
     def render_PUT(self, _):
         self.exClient.stopPolling()
-        return "Stopped"
+        return True
 
 
 application = Application("UR TCP Transfer Application")
-TCPServer(8880, Site(Transfer())).setServiceParent(application)
+TCPServer(8880, Site(API())).setServiceParent(application)
