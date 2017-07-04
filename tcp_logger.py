7ab10f3c03c46c86d771a62b82f23d43a1b73262
Peter Han <peter.han@intel.com>
2017-07-04 20:16:23 +0800

    Update tcp_logger.py

--- tcp_logger.py
+++ tcp_logger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import twisted.python.logfile
 
-def tcp_logger():
+def logger():
     f = logfile.LogFile("tcp.log", '/var/log/tcp/', rotateLength=10000000, maxRotatedFiles=50)
     log_observer = log.FileLogObserver(f)
     return log_observer.emit
